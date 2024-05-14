53efeea55f1a9307143e12abb63b6fbb48f15c0e
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2024-05-14 12:45:42 -0700

    Add section wrapper

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { styles } from '../styles';
+import { staggerContainer } from '../utils/motion';
+
+const SectionWrapper = () =>
+  function HOC() {
+
+return (
+  <motion.section>
+    <Component />
+  </motion.section>
+
+)
+    }
+  {
+  return(
+    <div>SectionWrapper</div>
+  )
+}
+
+export default SectionWrapper
