278cd9e42115d0b751de2931df0f1dd4c51d38d5
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2024-05-14 16:45:39 -0700

    Add experience text, icons, animation, and styling.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 import { SectionWrapper } from '../hoc';
 
-const ServiceCard = ({index, title, icon}) => {
+const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
@@ -21,45 +21,44 @@ const ServiceCard = ({index, title, icon}) => {
             speed: 450
           }}
           className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-          >
-            <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain'/>
           <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
-
-          </div>
+        </div>
       </motion.div>
     </Tilt>
-  )
-}
-
+  );
+};
 
 const About = () => {
   return (
     <>
-    <motion.div variants={textVariant()}>
-      <p className={styles.sectionSubText}>
-        Introduction</p>
-      <h2 className={styles.sectionHeadText}>
-        Overview</h2>
-    </motion.div>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          Introduction
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Overview
+        </h2>
+      </motion.div>
 
-    <motion.p
-      variants={fadeIn("", "", 0.1, 1)}
-      className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
-        I'm a skilled software developer with experince in Ruby and JavaScript,
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        I'm a skilled software developer with experience in Ruby and JavaScript,
         and expertise in frameworks like Rails, React, and Node.js.
         I'm a quick learner and collaborate closely with clients to create efficient,
-        scalabl, and user-friendly solutions that solve real-world problems.
-        Lets work together to bring your ideas to life!
-    </motion.p>
-    <div className='mt-20 flex flex-wrap gap-10'>
-      {services.map((service, index) => (
-        <ServiceCard key={service.title} index=
-        {index} {...service} />
-      ))}
+        scalable, and user-friendly solutions that solve real-world problems.
+        Let's work together to bring your ideas to life!
+      </motion.p>
 
-    </div>
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SectionWrapper (About, "about")
+export default SectionWrapper(About, "about");
